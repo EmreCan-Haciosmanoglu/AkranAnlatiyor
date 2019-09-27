62e5257b04394341e6a1c1592f6737aa3c18aedd
Emre Can HACIOSMANOĞLU <emrecan.haciosmanoglu@agu.edu.tr>
2019-09-27 13:22:27 +0300

    First implementation of login

--- routes/login.js
+++ routes/login.js
@@ -1,7 +1,46 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+
+const User = require('../models/User');
+
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 
 router.get('/', (req, res, next) => {
     return res.render('login', {data: 'This page is for loging in!'});
 });
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+passport.deserializeUser((id, done) => {
+  User.getUserById(id, (err, user) => {
+    done(err, user);
+  });
+});
+passport.use(new LocalStrategy((username, password, done) => {
+  User.getUserByUsername(username, (err, user) => {
+    if (err) {
+      throw err;
+    }
+    if (!user) {
+      return done(null, false, { message: "Unknown user" });
+    }
+
+    User.comparePassword(password, user.password, (err, isMatch) => {
+      if (err) return done(err);
+      if (isMatch) return done(null, user);
+      else return done(null, false, "Invalid Password");
+    });
+  });
+}));
+
+router.post('/login', passport.authenticate('local', { failureRedirect: "/users", failureFlash: "Invalid username or password" }), (req, res) => {
+  if (req.body.Redirect)
+    return res.redirect('' + req.body.Redirect);
+  else
+    return res.redirect('/home')
+});
+
 module.exports = router;

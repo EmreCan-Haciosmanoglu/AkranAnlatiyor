e78ae6d219d0ff4ae13f6fca33a8c07a0c74adb1
Emre Can HACIOSMANOĞLU <emrecan.haciosmanoglu@agu.edu.tr>
2019-09-27 23:13:35 +0300

    post request handler added

--- routes/register.js
+++ routes/register.js
@@ -1,7 +1,77 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const User = require('../models/User');
 
 router.get('/', (req, res, next) => {
-    return res.render('register', {data: 'This page is for registration!'});
+    return res.render('register', { data: 'This page is for registration!' });
+});
+
+router.post('/', (req, res, next) => {
+    const {
+        email,
+        password1,
+        firstname,
+        lastname,
+        username
+    } = req.body;
+
+    req.checkBody("firstname", "First name is required").notEmpty();
+    req.checkBody("lastname", "Last name is required").notEmpty();
+    req.checkBody("username", "Username is required").notEmpty();
+    req.checkBody("email", "E-mail is required").notEmpty();
+    req.checkBody("email", "E-mail is not valid").isEmail();
+    req.checkBody("password1", "Password is required").notEmpty();
+    req.checkBody("password2", "Passwords do not match").equals(req.body.password1);
+
+    var errors = req.validationErrors();
+    if (errors && errors.length > 0)
+        return res.redirect('/register'
+            + '?RegisterError=' + encodeURIComponent(errors[0].msg)
+            + ((firstname && firstname != "") ? '&firstname=' + encodeURIComponent(firstname) : '')
+            + ((lastname && lastname != "") ? '&lastname=' + encodeURIComponent(lastname) : '')
+            + ((username && username != "") ? '&username=' + encodeURIComponent(username) : '')
+            + ((email && email != "") ? '&email=' + encodeURIComponent(email) : '')
+        );
+
+    User.findOne({ 'username': username }, (user) => {
+        if (user) {
+            return res.redirect('/register'
+                + '?RegisterError=' + encodeURIComponent('Username is already taken')
+                + ((firstname && firstname != "") ? '&firstname=' + encodeURIComponent(firstname) : '')
+                + ((lastname && lastname != "") ? '&lastname=' + encodeURIComponent(lastname) : '')
+                + ((email && email != "") ? '&email=' + encodeURIComponent(email) : '')
+            );
+        }
+        User.findOne({ 'email': email }, (user) => {
+            if (user) {
+                return res.redirect('/register'
+                    + '?RegisterError=' + encodeURIComponent('Email is already taken')
+                    + ((firstname && firstname != "") ? '&firstname=' + encodeURIComponent(firstname) : '')
+                    + ((lastname && lastname != "") ? '&lastname=' + encodeURIComponent(lastname) : '')
+                    + ((username && username != "") ? '&username=' + encodeURIComponent(username) : '')
+                );
+            }
+            bcrypt.hash(password1, 10).then((hash) => {
+                const user = new User({
+                    email: email,
+                    firstname: firstname,
+                    lastname: lastname,
+                    username: username,
+                    password: hash
+                });
+
+                user.save().then((data) => {
+                    if (data)
+                        return res.redirect('/login');
+                }).catch((error) => {
+                    console.error(error);
+                    return res.redirect('/register');
+                });
+            });
+        });
+    });
 });
 module.exports = router;

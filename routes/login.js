2b3b1297decd9a2d4f291c998a324681f2ad4e8a
Emre Can HACIOSMANOĞLU <emrecan.haciosmanoglu@agu.edu.tr>
2019-09-29 00:04:00 +0300

    Authentication is added to login page to redirect the user to the main page if user already logged in

--- routes/login.js
+++ routes/login.js
@@ -7,8 +7,8 @@ const User = require('../models/User');
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 
-router.get('/', (req, res, next) => {
-    return res.render('login', {data: 'This page is for loging in!'});
+router.get('/', ensureNotAuthenticated, (req, res, next) => {
+  return res.render('login', { data: 'This page is for loging in!' });
 });
 
 passport.serializeUser((user, done) => {
@@ -36,11 +36,18 @@ passport.use(new LocalStrategy((username, password, done) => {
   });
 }));
 
-router.post('/', passport.authenticate('local', { failureRedirect: "/login", failureFlash: "Invalid username or password" }), (req, res) => {
+router.post('/', ensureNotAuthenticated, passport.authenticate('local', { failureRedirect: "/login", failureFlash: "Invalid username or password" }), (req, res) => {
   if (req.body.Redirect)
     return res.redirect('' + req.body.Redirect);
   else
     return res.redirect('/');
 });
+
+function ensureNotAuthenticated(req, res, next) {
+  if (!req.isAuthenticated())
+    return next();
+
+  return res.redirect('/home');
+}
 
 module.exports = router;

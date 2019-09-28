e90ecc114f26392aa9677d783f150d967ff58866
Emre Can HACIOSMANOĞLU <emrecan.haciosmanoglu@agu.edu.tr>
2019-09-29 00:02:36 +0300

    Authentication is added to admin page to force user to login

--- routes/admin.js
+++ routes/admin.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
+router.get('/', ensureAuthenticated, (req, res, next) => {
     return res.render('admin', {data: 'This page is for admin!'});
 });
+
+function ensureAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) return next();
+    var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
+    return res.redirect('/login'
+        + '?LoginError=' + encodeURIComponent('You have to Login to see the page')
+        + '&Redirect=' + encodeURIComponent(fullUrl)
+    );
+}
 module.exports = router;

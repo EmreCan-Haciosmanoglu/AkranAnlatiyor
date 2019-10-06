1b5ef22b66e817913f6284d82b02176b9493265d
Emre Can HACIOSMANOĞLU <emrecan.haciosmanoglu@agu.edu.tr>
2019-10-06 23:03:59 +0300

    Post request is implemented(Need testing a lot)

--- routes/reset.js
+++ routes/reset.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
+const User = require('../models/User');
 const Link = require('../models/PasswordReset');
 
 router.get('/', ensureNotAuthenticated, (req, res, next) => {
@@ -21,14 +23,59 @@ router.get('/', ensureNotAuthenticated, (req, res, next) => {
             'PlaceholderConfirmPassword': 'Confirm New Password',
             'PlaceholderPIN': 'Enter PIN',
             'btnReset': 'Reset',
-            'BackToLogin': 'Go To Login Page'
+            'BackToLogin': 'Go To Login Page',
+            'Token': token
         };
         return res.render('reset', handlebarsData);
     });
 });
 
 router.post('/', ensureNotAuthenticated, (req, res, next) => {
+    const token = req.body.Token;
+    if (!token || token == "")
+        return res.redirect('/login');
 
+    Link.getLinkByToken(token, (err, link) => {
+        if (err)
+            return res.redirect('/login');
+        if (!link)
+            return res.redirect('/login');
+
+        const {
+            password1,
+            password2,
+            PIN
+        } = req.body;
+
+        req.checkBody("PIN", "PIN is required").notEmpty();
+        req.checkBody("password1", "Password is required").notEmpty();
+        req.checkBody("password2", "Passwords do not match").equals(req.body.password1);
+
+        var errors = req.validationErrors();
+        if (errors && errors.length > 0)
+            return res.redirect('/register'
+                + '?Error=' + encodeURIComponent(errors[0].msg)
+                + '&Token=' + encodeURIComponent(token)
+            );
+        bcrypt.hash(PIN, 10).then((pinHash) => {
+            Link.findOne({ token: token, pin: pinHash }, (err, result) => {
+                if (err)
+                    return res.redirect('/login');
+                if (!result)
+                    return res.redirect('/login');
+                if (result.isReseted)
+                    return res.redirect('/login');
+                bcrypt.hash(password1, 10).then((passwordHash) => {
+                    User.findOne({ email: result.email }, (err, user) => {
+                        user.password = passwordHash;
+                        user.save();
+                        result.isReseted = true;
+                        result.save();
+                    });
+                });
+            });
+        });
+    });
 });
 
 function ensureNotAuthenticated(req, res, next) {

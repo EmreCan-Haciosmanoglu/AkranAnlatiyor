72de074c17c2cef63b43f76b924708662c255df1
Emre Can HACIOSMANOĞLU <emrecan.haciosmanoglu@agu.edu.tr>
2019-10-16 13:36:41 +0300

    First IMp of forgot page

--- routes/forgot.js
+++ routes/forgot.js
@@ -1,8 +1,30 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-    return res.render('forgot');
+router.get('/', ensureNotAuthenticated, (req, res, next) => {
+
+    var handlebarsData = {
+        'ForgotTitle': 'Forgot Password?',
+        'PlaceholderEmail': 'Email',
+        'btnForgot': 'Send',
+        'BackToLogin': 'Go To Login Page',
+    };
+    return res.render('forgot', handlebarsData);
 });
+
+router.post('/', ensureNotAuthenticated, (req, res, next) => {
+    //check database for email
+
+    //Create a token for reseting
+
+    //send an email to that email
+});
+
+function ensureNotAuthenticated(req, res, next) {
+    if (!req.isAuthenticated())
+        return next();
+
+    return res.redirect('/home');
+}
 
 module.exports = router;

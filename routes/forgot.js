5a0ca5c97b959226910630866182f4d5cc3c0ead
Emre Can HACIOSMANOĞLU <emrecan.haciosmanoglu@agu.edu.tr>
2019-10-16 14:36:51 +0300

    Needed Feature is implemented but needs a test

--- routes/forgot.js
+++ routes/forgot.js
@@ -1,5 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const nodemailer = require('nodemailer');
+const SecretKey = require('./../private/SecretKey');
+const Passwords = require('./../private/Passwords');
+
+const User = require('../models/User');
+const Link = require('../models/PasswordReset');
 
 router.get('/', ensureNotAuthenticated, (req, res, next) => {
 
@@ -13,6 +21,61 @@ router.get('/', ensureNotAuthenticated, (req, res, next) => {
 });
 
 router.post('/', ensureNotAuthenticated, (req, res, next) => {
+    const email = req.body.email;
+    Link.findOne({ email: email, isReseted: false }, (err, link) => {
+        if (err)
+            console.log(err);
+        if (link)
+            return res.redirect('/forgot');
+
+        User.findOne({ email: email }, (err, user) => {
+            if (err)
+                console.log(err);
+            if (!user)
+                return res.redirect('/forgot');
+
+            const pin = Math.floor(Math.random() * 1000 + 1000);
+            bcrypt.hash('' + Date.now() + '' + SecretKey.Key + '' + pin + '' + email, 10).then((token) => {
+                link = new Link({
+                    email: email,
+                    token: token,
+                    pin: pin,
+                    isReseted: false
+                });
+
+                link.save().then(async (data) => {
+                    if (data) {
+
+                        const htmlBody = 'This is your link and pin to reset your password\n www.akrananlatiyor.com/reset?Token=' + token + ' \nPIN: ' + pin;
+
+                        const transporter = nodemailer.createTransport({
+                            host: 'smtp.gmail.com',
+                            port: 587,
+                            auth: {
+                                user: 'AkranAnlatiyor@gmail.com',
+                                pass: Passwords.password
+                            }
+                        });
+
+                        let info = await transporter.sendMail({
+                            from: '"Support Team 👻" <AkranAnlatiyor@gmail.com>', // sender address
+                            to: email, // list of receivers
+                            subject: 'Password Reset', // Subject line
+                            text: 'Did you forget already?', // plain text body
+                            html: htmlBody // html body
+                        });
+
+                        console.log('Message sent: %s', info.messageId);
+                        return res.redirect('/forgot');
+                    }
+                }).catch((error) => {
+                    console.error(error);
+                    return res.redirect('/forgot');
+                });
+
+            });
+        });
+    });
     //check database for email
 
     //Create a token for reseting

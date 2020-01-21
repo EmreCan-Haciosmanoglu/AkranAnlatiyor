5f31722b19f7bd62ea4fb19f1a96adb5c45ed574
Emre Can HACIOSMANOĞLU <emrecan.haciosmanoglu@agu.edu.tr>
2020-01-21 13:48:03 +0300

    Redirection of Senior user is implemented

--- routes/mainpage.js
+++ routes/mainpage.js
@@ -1,25 +1,33 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', ensureAuthenticated, (req, res, next) => {
-    var num = Math.random(Math.random());
-    if (num == 1)
-        return res.render('freshman', { data: 'This is a page for freshman students who are seeking for information!' });
-    else
-        return res.render('senior', { data: 'This is a page for senior students who are looking for students to share information!' });
+const User = require('../models/User');
+const Senior = require('../models/Senior');
+
+router.get('/', ensureAuthenticated, (req, res) => {
+    User.findOne({username : req.username}, (error,user)=>{
+        if (error)
+            return res.redirect('/login?Error=' + encodeURIComponent(error));
+        Senior.findOne({email:user.email}, (error, senior)=>{
+            if (error)
+                return res.redirect('/login?Error=' + encodeURIComponent(error));
+            if(senior)
+                return res.redirect('/senior');
+            return res.render('freshman', { data: 'This is a page for freshman students who are seeking for information!' });
+        }) ;
+    });
 });
 
 router.get('/logout', (req, res) => {
   req.logout();
-  res.redirect('/login?Success=' + encodeURIComponent('Successfully Loged out!'));
+  res.redirect('/login?Success=' + encodeURIComponent('Successfully Logged out!'));
 });
 
 function ensureAuthenticated(req, res, next) {
     if (req.isAuthenticated()) return next();
     var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
     return res.redirect('/login'
-        + '?Error=' + encodeURIComponent('You have to Login to see the page')
-        + '&Redirect=' + encodeURIComponent(fullUrl)
+        + '?Redirect=' + encodeURIComponent(fullUrl)
     );
 }
 module.exports = router;

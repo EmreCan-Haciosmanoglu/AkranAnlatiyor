ab8442fb2e5025a81abd8f7887d506cf8b2b228e
Emre Can HACIOSMANOĞLU <emrecan.haciosmanoglu@agu.edu.tr>
2020-01-21 14:40:34 +0300

    text is added to page seperatelly

--- routes/senior.js
+++ routes/senior.js
@@ -2,7 +2,66 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', ensureAuthenticated, (req, res, next) => {
-    return res.render('senior', { data: 'This is a page for senior students who are looking for students to share information!' });
+
+    var handlebarsData = {
+        'SideNav': {
+            'FullName': 'Mustafa Kemal Özdemir',
+            'Major': 'Computer Engineering',
+            'ClientCount': '5 Akran',
+            'Rating': '1.5/10'
+        },
+        'NavBar': [
+            {
+                'Class': 'active',
+                'Link': '/',
+                'Text': 'Active Meetings'
+            },
+            {
+                'Link': '/',
+                'Text': 'Pending Meetings'
+            },
+            {
+                'Link': '#',
+                'Text': 'Pending'
+            },
+            {
+                'Link': '#',
+                'Text': 'Students'
+            },
+            {
+                'Link': '#',
+                'Text': 'Calendar'
+            },
+            {
+                'Link': '#',
+                'Text': 'Account'
+            }
+        ],
+        'Title': 'Active Meetings',
+        'TableHead': [
+            'Name',
+            'Department',
+            'Location',
+            'Topic',
+            'Date DD/MM',
+            'Edit'
+        ]
+    };
+    var data = [[
+        'Mustafa Kemal Özdemir',
+        'Computer Engineering',
+        'B212 Steel Building',
+        'Life in Kayseri',
+        '02/01 9:15'
+    ],[
+        'Emre Can Hacıosmanoğlu',
+        'Computer Engineering',
+        'BA12',
+        'Life in Kayseri',
+        '10/05 9:15'
+    ]];
+    handlebarsData['Table'] = data;
+    return res.render('senior', handlebarsData);
 });
 
 function ensureAuthenticated(req, res, next) {

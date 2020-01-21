24711d7b2354f9acef49df4788daa9d195ea2e77
Emre Can HACIOSMANOĞLU <emrecan.haciosmanoglu@agu.edu.tr>
2020-01-21 13:47:06 +0300

    Authentication is added

--- routes/senior.js
+++ routes/senior.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
+router.get('/', ensureAuthenticated, (req, res, next) => {
     return res.render('senior', { data: 'This is a page for senior students who are looking for students to share information!' });
 });
+
+function ensureAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) return next();
+    var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
+    return res.redirect('/login'
+        + '?Error=' + encodeURIComponent('You have to Login to see the page')
+        + '&Redirect=' + encodeURIComponent(fullUrl)
+    );
+}
 
 module.exports = router;

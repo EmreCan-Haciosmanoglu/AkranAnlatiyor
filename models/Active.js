f9a443e69e871ee4039b5e0a23440c62a44d71a1
Emre Can HACIOSMANOĞLU <emrecan.haciosmanoglu@agu.edu.tr>
2020-02-20 18:52:25 +0300

    Schema is added for active meetings

---
+++ models/Active.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const activeShema = new Schema({
+    seniorEmail: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    freshmanEmail: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    freshmanMajor: {
+        type: String,
+        required: true
+    },
+    place: {
+        type: String,
+        required: true
+    },
+    topic: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: String,
+        required: true
+    }
+});
+module.exports = mongoose.model('active', seniorShema);
